7ae41adc9d27d7dfc1f41c20a1f0ebb40c8a47b6
deepakjnv880 <b17039@students.iitmandi.ac.in>
2019-11-01 23:37:40 +0530

    updated

--- part b/part 1/main.py
+++ part b/part 1/main.py
@@ -4,14 +4,14 @@ import random
 import matplotlib.pyplot as plt
 from PIL import Image
 #manhattan distance is used as distance measure
-error_allowed=80
+error_allowed=60
 K=7
 
 def calculate_manhattan_distance(first_pixel,second_pixel):
-    # print('####### ############ ',first_pixel,second_pixel)
+    # print('####### ############ ',first_pixel,second_pixel[0])
     distance=0
     for i in range(3):
-        distance=distance+abs(first_pixel[i]-second_pixel[i])
+        distance=distance+abs(int(first_pixel[i])-int(second_pixel[i]))
     # print("hi am ended,distance",distance)
     return distance
 
@@ -29,7 +29,9 @@ def assign_cluster(image_pixel,k_mean):
 def assign_k_mean1(image):
     k_mean=[]
     for i in range(K):
-        k_mean.append(random.choice(image))
+        x=random.randrange(0, 300, 5)
+        y=random.randrange(0, 300, 5)
+        k_mean.append(image[x][y])
     return k_mean
 
 def assign_k_mean2():
@@ -52,10 +54,10 @@ def main():
     # print(narray[0,].reshape(imgage.shape[0:2]))
     # print(np.shape(np.array(image)))
 
-    k_mean=assign_k_mean2()
+    k_mean=assign_k_mean1(image)
     # print(k_mean)
     new_distortion_value=0
-    old_distortion_value=100
+    old_distortion_value=7000
     k_cluster=[[] for i in range(K)]
     while(error_allowed<abs(old_distortion_value-new_distortion_value)):
         old_distortion_value=new_distortion_value
@@ -70,27 +72,24 @@ def main():
                 new_distortion_value=new_distortion_value+min_distance
 
         ######### improving k means #########
-        # deepak=8
+        deepak=8
         for i in range(K):
-            print("this is my cluster: \n\n\n",int(k_cluster[i]))
+            # print("this is my cluster: \n\n\n",k_cluster[i])
             k_mean[i]=np.mean(np.array(k_cluster[i]), axis=0)
 
         print('Error is ',abs(old_distortion_value-new_distortion_value))
-
-    # print(k_cluster)
     dim=300
     for x in range(K):
-
+        # print('################ \n ',k_cluster[x],'\n#############')
         data = np.zeros((dim, dim, 3), dtype=np.uint8)
         for i in range(dim):
             for j in range(dim):
                 data[i][j]=[255,255,255]
         count=0
         check=False
-        for i in range(0,(2*dim)+1,2):
-            for j in range(0,(2*dim)+1,2):
-                print(i,j)
-                print(count)
+        for i in range(0,(dim),1):
+            for j in range(0,(dim),1):
+                # print(i,j)
                 data[i][j]=list(np.array(k_cluster[x][count]))
                 count=count+1
                 if count==len(k_cluster[x]):
@@ -99,8 +98,8 @@ def main():
             if check:
                 break
         img = Image.fromarray(data, 'RGB')
-        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-        img.save('k clustered pixel image/output'+str(x+1)+'.png')
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img.save('k clustered pixel image/'+str(x+1)+'.jpg')
         # img.show()
 
 if __name__ == '__main__':
